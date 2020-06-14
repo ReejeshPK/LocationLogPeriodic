d7924198bac8fd54b491a67a1f3e0926982b6f2b
Reejesh PK <pkreejeshpk203@gmail.com>
2020-06-14 16:20:12 +0530

    get current location

--- app/src/main/java/com/example/mylocationlogapp/activities/PIckCurrentLocationMapsActivity.kt
+++ app/src/main/java/com/example/mylocationlogapp/activities/PIckCurrentLocationMapsActivity.kt
@@ -1,19 +1,38 @@
 package com.example.mylocationlogapp.activities
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.example.mylocationlogapp.BaseActivity
+import com.example.mylocationlogapp.BuildConfig
 import com.example.mylocationlogapp.R
+import com.google.android.gms.location.FusedLocationProviderClient
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_p_ick_current_location_maps.*
 
-class PIckCurrentLocationMapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class PIckCurrentLocationMapsActivity : BaseActivity(), OnMapReadyCallback {
 
+    private val TAG: String = PIckCurrentLocationMapsActivity::class.java.toString()
     private lateinit var mMap: GoogleMap
+    private var latLng: LatLng? = null
+    private val REQUEST_PERMISSIONS_REQUEST_CODE: Int = 100;
+    private var mFusedLocationClient: FusedLocationProviderClient? = null
+    protected var mLastLocation: Location? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,8 +56,148 @@ class PIckCurrentLocationMapsActivity : AppCompatActivity(), OnMapReadyCallback 
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        /* val sydney = LatLng(-34.0, 151.0)
+         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))*/
+
+        mMap.setOnCameraIdleListener {
+            logd(TAG, "change")
+            latLng = mMap.cameraPosition.target
+            logd(TAG, "change${latLng}")
+            myCurrentLocationText.text = "Lat:${latLng?.latitude}\nLng:${latLng?.longitude}"
+        }
+    }
+
+
+    private fun checkPermissions(): Boolean {
+        val permissionState = ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        return permissionState == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startLocationPermissionRequest() {
+        ActivityCompat.requestPermissions(
+            this@PIckCurrentLocationMapsActivity,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+            REQUEST_PERMISSIONS_REQUEST_CODE
+        )
+    }
+
+    private fun requestPermissions() {
+        val shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+
+        // Provide an additional rationale to the user. This would happen if the user denied the
+        // request previously, but didn't check the "Don't ask again" checkbox.
+        if (shouldProvideRationale) {
+            logd(TAG, "Displaying permission rationale to provide additional context.")
+
+            showSnackbar(R.string.permission_rationale, android.R.string.ok,
+                View.OnClickListener {
+                    // Request permission
+                    startLocationPermissionRequest()
+                })
+
+        } else {
+            logd(TAG, "Requesting permission")
+            // Request permission. It's possible this can be auto answered if device policy
+            // sets the permission in a given state or the user denied the permission
+            // previously and checked "Never ask again".
+            startLocationPermissionRequest()
+        }
+    }
+
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        logd(TAG, "onRequestPermissionResult")
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.size <= 0) {
+                // If user interaction was interrupted, the permission request is cancelled and you
+                // receive empty arrays.
+                logd(TAG, "User interaction was cancelled.")
+            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted.
+                getLastLocation()
+            } else {
+                // Permission denied.
+
+                // Notify the user via a SnackBar that they have rejected a core permission for the
+                // app, which makes the Activity useless. In a real app, core permissions would
+                // typically be best requested during a welcome-screen flow.
+
+                // Additionally, it is important to remember that a permission might have been
+                // rejected without asking the user for permission (device policy or "Never ask
+                // again" prompts). Therefore, a user interface affordance is typically implemented
+                // when permissions are denied. Otherwise, your app could appear unresponsive to
+                // touches or interactions which have required permissions.
+                showSnackbar(R.string.permission_denied_explanation, R.string.settings,
+                    View.OnClickListener {
+                        // Build intent that displays the App settings screen.
+                        val intent = Intent()
+                        intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                        val uri = Uri.fromParts(
+                            "package",
+                            BuildConfig.APPLICATION_ID, null
+                        )
+                        intent.data = uri
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        startActivity(intent)
+                    })
+            }
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+
+        if (!checkPermissions()) {
+            requestPermissions()
+        } else {
+            getLastLocation()
+        }
+    }
+
+    private fun getLastLocation() {
+        mFusedLocationClient!!.lastLocation
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful && task.result != null) {
+                    mLastLocation = task.result
+
+                    /* mLatitudeText!!.setText(
+                         mLatitudeLabel+":   "+
+                                 (mLastLocation )!!.latitude)
+                     mLongitudeText!!.setText(mLongitudeLabel+":   "+
+                             (mLastLocation )!!.longitude)*/
+
+                    val curloc = LatLng(mLastLocation!!.latitude, mLastLocation!!.longitude)
+                    mMap.addMarker(MarkerOptions().position(curloc).title("Marker in Current Location"))
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(curloc))
+
+                } else {
+                    loge(TAG, "getLastLocation:exception" + task.exception)
+                    showMessage(getString(R.string.no_location_detected))
+                }
+            }
+    }
+
+
+    private fun showMessage(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+    }
+    private fun showSnackbar(mainTextStringId: Int, actionStringId: Int,
+                             listener: View.OnClickListener) {
+        Toast.makeText(this, getString(mainTextStringId), Toast.LENGTH_LONG).show()
     }
 }
+
+
