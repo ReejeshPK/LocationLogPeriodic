256f07e94a2ef43bf9e46d6ebf66817efcce7f7c
Reejesh PK <pkreejeshpk203@gmail.com>
2020-06-17 01:00:58 +0530

    swap try in db

--- app/src/main/java/com/example/mylocationlogapp/activities/LocationListActivity.kt
+++ app/src/main/java/com/example/mylocationlogapp/activities/LocationListActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.ItemTouchHelper
-
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mylocationlogapp.BaseActivity
@@ -16,11 +15,11 @@ import com.example.mylocationlogapp.listeners.ItemMoveCallbackListener
 import com.example.mylocationlogapp.listeners.OnStartDragListener
 import com.example.mylocationlogapp.modal.MyLocationModal
 import io.realm.Realm
-
 import kotlinx.android.synthetic.main.activity_location_list.*
 import kotlinx.android.synthetic.main.content_location_list.*
 
-class LocationListActivity : BaseActivity(), OnStartDragListener {
+
+class LocationListActivity : BaseActivity(), OnStartDragListener,MyLocationAdapter.MyRecyclerEventsListener {
 
     private lateinit var realm: Realm
     private val TAG = LocationListActivity::class.java.simpleName
@@ -66,7 +65,7 @@ class LocationListActivity : BaseActivity(), OnStartDragListener {
             var sizeList:Int= myLocationList?.size!!;
             if(sizeList > 0) {
 
-                adapter = MyLocationAdapter(this@LocationListActivity,this@LocationListActivity)
+                adapter = MyLocationAdapter(this@LocationListActivity,this@LocationListActivity,this@LocationListActivity)
                 /**Start of drag and drop*/
                 val callback: ItemTouchHelper.Callback = ItemMoveCallbackListener(adapter)
                 touchHelper = ItemTouchHelper(callback)
@@ -124,4 +123,81 @@ class LocationListActivity : BaseActivity(), OnStartDragListener {
     override fun onStartDrag(viewHolder: RecyclerView.ViewHolder) {
         touchHelper.startDrag(viewHolder)
     }
+
+
+
+    /**Swap in db and reload*/
+
+    override fun onSwapPerformed(fromPos: Int, toPos: Int,myLocationListLocal:List<MyLocationModal>) {
+        //todo: need async task and queing mechanism ?
+        //realm.insertOrUpdate(myLocationList)
+       /* realm.executeTransactionAsync({
+            //it.insertOrUpdate(myLocationList)
+            val idFrom=myLocationListLocal.get(fromPos).id
+            val dataFrom=myLocationListLocal.get(fromPos)
+            val idTo=myLocationListLocal.get(toPos).id
+            val dataTo=myLocationListLocal.get(toPos)
+
+            //swap
+            val tmpObj=dataFrom
+            tmpObj.id=dataFrom.id
+            tmpObj.user_id=dataTo.user_id
+            tmpObj.latitude=dataTo.latitude
+            tmpObj.longitude=dataTo.longitude
+            tmpObj.dateTime=dataTo.dateTime
+
+            it.insertOrUpdate(tmpObj)
+
+            //next
+            dataTo.user_id=dataFrom.user_id
+            dataTo.latitude=dataFrom.latitude
+            dataTo.longitude=dataFrom.longitude
+            dataTo.dateTime=dataFrom.dateTime
+
+            it.insertOrUpdate(dataTo)
+
+        },{
+            logd(TAG,"success")
+        },{
+            loge(TAG,"err:"+it.message)
+        })*/
+
+        val idFrom=myLocationListLocal.get(fromPos).id
+        val dataFrom=myLocationListLocal.get(fromPos)
+        val idTo=myLocationListLocal.get(toPos).id
+        val dataTo=myLocationListLocal.get(toPos)
+        val tmpObj=dataFrom
+
+       /* realm.executeTransaction {
+            //swap
+            //tmpObj.id=dataFrom.id //Primary key field 'id' cannot be changed after object was created.
+            dataFrom.user_id=dataTo.user_id //Cannot modify managed objects outside of a write transaction.
+            dataFrom.latitude=dataTo.latitude
+            dataFrom.longitude=dataTo.longitude
+            dataFrom.dateTime=dataTo.dateTime
+
+            realm.insertOrUpdate(tmpObj)
+
+            //next
+            dataTo.user_id=tmpObj.user_id
+            dataTo.latitude=tmpObj.latitude
+            dataTo.longitude=tmpObj.longitude
+            dataTo.dateTime=tmpObj.dateTime
+
+            realm.insertOrUpdate(dataTo)
+        }*/
+
+
+
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (realm != null) { // don't trust old devices
+            realm.close()
+        }
+    }
+
 }
--- app/src/main/java/com/example/mylocationlogapp/adapters/MyLocationAdapter.kt
+++ app/src/main/java/com/example/mylocationlogapp/adapters/MyLocationAdapter.kt
@@ -5,12 +5,14 @@ import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.MotionEvent
+import android.view.MotionEvent.ACTION_CANCEL
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mylocationlogapp.MyApplicationClass
 import com.example.mylocationlogapp.R
 import com.example.mylocationlogapp.activities.MapsActivityWithPlay
 import com.example.mylocationlogapp.helper.MyConstants
@@ -20,17 +22,25 @@ import com.example.mylocationlogapp.modal.MyLocationModal
 import java.text.SimpleDateFormat
 import java.util.*
 
-class MyLocationAdapter(val context :Context,
-                        private val startDragListener: OnStartDragListener) :
+class MyLocationAdapter(
+    val context: Context,
+    private val startDragListener: OnStartDragListener,
+    private val myRecyclerEventsListener: MyRecyclerEventsListener
+) :
     RecyclerView.Adapter<MyLocationAdapter.MyViewHolder>(), ItemMoveCallbackListener.Listener {
 
-    private var myLocationList = emptyList<MyLocationModal>().toMutableList() //done for drag and drop requires this
+    interface MyRecyclerEventsListener {
+        fun onSwapPerformed(fromPos: Int, toPos: Int,myLocationList:List<MyLocationModal>)
+    }
+
+    private var myLocationList =
+        emptyList<MyLocationModal>().toMutableList() //done for drag and drop requires this
 
     fun setList(myLocationListFromActivity: List<MyLocationModal>) {
         myLocationList.addAll(myLocationListFromActivity)
     }
 
-    val simpleDateFormat= SimpleDateFormat(MyConstants.MY_DATE_FORMAT,MyConstants.MYGLOBAL_LOCALE)
+    val simpleDateFormat = SimpleDateFormat(MyConstants.MY_DATE_FORMAT, MyConstants.MYGLOBAL_LOCALE)
 
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var myLocationText: TextView? = null
@@ -71,17 +81,25 @@ class MyLocationAdapter(val context :Context,
         holder.dateTime?.setText(simpleDateFormat.format(oneLocation?.dateTime))
 
         holder.myLocationLayout?.setOnClickListener({
-                val intent = Intent(context,MapsActivityWithPlay::class.java)
+            val intent = Intent(context, MapsActivityWithPlay::class.java)
             if (oneLocation != null) {
-                intent.putExtra(MyConstants.INTENT_EXTRA_LOCATION_POS,oneLocation.id)
+                intent.putExtra(MyConstants.INTENT_EXTRA_LOCATION_POS, oneLocation.id)
             }
             context.startActivity(intent)
         })
 
         holder.myDragIcon?.setOnTouchListener { _, event ->
+            MyApplicationClass.globalLog("MyLocationAdapter","action:"+event.action)
             if (event.action == MotionEvent.ACTION_DOWN) {
+                MyApplicationClass.globalLog("MyLocationAdapter", "onTouchDown")
                 this.startDragListener.onStartDrag(holder)
-            }
+                return@setOnTouchListener true
+            }/* else if ((event.action == MotionEvent.ACTION_UP)||(event.action==ACTION_CANCEL)) {
+                //user released the touch
+                //todo: better place to update the list - events not registering correctly, only action_cancel is called
+                MyApplicationClass.globalLog("MyLocationAdapter", "onTouchUp")
+                return@setOnTouchListener true
+            }*/
             return@setOnTouchListener true
         }
     }
@@ -93,10 +111,20 @@ class MyLocationAdapter(val context :Context,
     }
 
     override fun onRowMoved(fromPosition: Int, toPosition: Int) {
+        MyApplicationClass.globalLog(
+            "MyLocationAdapter",
+            "onRowMoved:" + fromPosition + " to:" + toPosition
+        )
+        //todo swap in db also - doing it here will be inefficient, since this is called lot of times so
+        //todo do it when user releases his touch - ti also does not work properly, so doing here itself
+        myRecyclerEventsListener.onSwapPerformed(fromPosition, toPosition,myLocationList)
+
+        //todo analyze why for loop, we could have done wihout it too, i think it is becasue it keeps moving -yes, for each movement, we are swapping up or down
         if (fromPosition < toPosition) {
             for (i in fromPosition until toPosition) {
                 Collections.swap(myLocationList, i, i + 1)
             }
+
         } else {
             for (i in fromPosition downTo toPosition + 1) {
                 Collections.swap(myLocationList, i, i - 1)
@@ -104,8 +132,10 @@ class MyLocationAdapter(val context :Context,
         }
         notifyItemMoved(fromPosition, toPosition)
     }
+
     override fun onRowSelected(itemViewHolder: MyViewHolder) {
     }
+
     override fun onRowClear(itemViewHolder: MyViewHolder) {
     }
 }
